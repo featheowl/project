013f9e2ad627e39932f8735e4b7a33945fb8fa77
Kolya <featheowl@gmail.com>
2023-06-19 23:59:31 +0300

    home test

---
+++ reverse_str.py
@@ -0,0 +1,9 @@
+def str_reverse(s):
+    s=s.replace(' ','')
+    if list(s)==list(reversed(s)) :
+        return True
+    else:
+        return False
+first = str_reverse(' лепсспел')
+second = str_reverse('hello world')
+print(x)
